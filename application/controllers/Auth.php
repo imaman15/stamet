175616dbbbfec2b61f1f65c9c0ec6494b63fb597
Imam Agustian Nugraha <imamagustiannugraha@ymail.com>
2019-06-10 10:34:36 +0700

    Persiapan

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,7 +6,16 @@ class Auth extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('auth/login', $data, FALSE);
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+        
+    }
+    
+    public function registration(){
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/registration');
+        $this->load->view('templates/auth_footer');
         
     }
 
